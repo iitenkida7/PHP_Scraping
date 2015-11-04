9cbb5578b7ebe8281c6583e11146f8721b9dd948
Esfahan <calito0131@gmail.com>
2015-11-04 14:51:01 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # PHP_Scraping
-PHP_Scraping
+php library of web scraping
+
+###Scraping.php
+  This is for scraping from html.<br>
+  <br>
+  _sample) getting html without proxy_<br>
+  
+     $scraping = new Scraping();
+     
+     // get html data
+     $url = 'http://xxxx.com';
+     $header = array('Content-Type: text/html', 'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7');
+     $html = $scraping->getContentsByCurl($url, $header);
+     
+     print $html;
+
+  
+<br>
+  _sample) getting html with proxy_<br>
+  
+     $scraping = new Scraping();
+     
+     // get html data
+     $url = 'http://xxxx.com';
+     $header = array('Content-Type: text/html', 'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7');
+     $proxy = true;
+     $proxy_port = 8080;
+     $proxy_url = 'tcp://xxxxx.net';
+     // if you have post_data
+     $post_data = array('key' => 'value');
+     $html = $scraping->getContentsByCurl($url, $header, $post_data, $proxy, $proxy_port, $proxy_url);
+     
+     print $html;
+
+  
+<br>
+  _sample) extracting text from $start to $end(preg_match_all)_<br>
+  
+     $scraping = new Scraping();
+     
+     $target_text = '<a href="http://aaa.com">aaa</a>';
+     $target_text .= '<a href="http://bbb.com">bbb</a>';
+     $target_text .= '<a href="http://ccc.com">ccc</a>';
+     $target_text .= '<a href="http://ddd.com">ddd</a>';
+     
+     $start = '<a href="';
+     $end = '"';
+     $preg_match_all=true;
+     $result = $scraping->extractSpecifiedTextRange($start, $end, $target_text, $preg_match_all);
+     
+     print_r($result);
+     /**
+     * result
+     *
+     * Array(
+     *		[0] => http://aaa.com
+     *		[1] => http://bbb.com
+     *		[2] => http://ccc.com
+     *		[3] => http://ddd.com
+     *	)
+     */
+
---
+++ Scraping.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+* It's for scraping
+*
+* @version	1.0
+*/
+class Scraping{
+	// timeout of connection
+	private $connect_timeout = 60;
+	// timeout of requesting
+	private $timeout = 60;
+	// default user agent
+	private $ua = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7';
+
+	function __construct(){
+	}
+
+	/**
+	* curl
+	*
+	* @param str $url
+	* @param str $header
+	* @param boolean $proxy //true => use, false => not use
+	* @param int $proxy_port
+	* @param str $proxy_url
+	* @return json|xml|text
+	*/
+	public function getContentsByCurl($url, $header = '', $post_data=array(), $proxy=false, $proxy_port='', $proxy_url=''){
+		$ch = curl_init($url);
+
+		// set return data of curl_exec()
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		
+		// post
+		if(!empty($post_data)){
+			// TRUE => sending HTTP POST
+			curl_setopt($ch, CURLOPT_POST, TRUE);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS , $post_data);
+                }
+
+		// set default user agent
+		if(empty($header)){
+			$header = array('Content-Type: text/html', 'User-Agent: '.$this->ua);	
+		}
+		// HTTP HEADER ex) array('Content-type: text/plain', 'Content-length: 100')
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+		// timeout of connection
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->connect_timeout);
+		// timeout of response
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+
+		// use proxy
+		if($proxy==true){
+			// "Off" is better.
+			//curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, TRUE);
+			// port
+			curl_setopt($ch, CURLOPT_PROXYPORT, $proxy_port);
+			// proxy url
+			curl_setopt($ch, CURLOPT_PROXY, $proxy_url);
+		}
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		return $result;
+	}
+
+	/**
+	* changing xml to array
+	*
+	* @param str $xml //xml data
+	* @return array
+	*/
+	public function xmlToArray($xml){
+		return json_decode(json_encode(simplexml_load_string($xml)), true);
+	}
+
+	/**
+	* extracting the ragne text from $start to $end
+	*
+	* @param str $start //start
+	* @param str $end //end
+	* @param str $target_text //extracted text
+	* @param boolean $preg_match_all //true => preg_match_all, false => preg_match
+	* @param array $not_escape 
+	*		$not_escape['start']==true => $start is not applied preg_quote()
+	*		$not_escape['end']==true => $end is not applied preg_quote()
+	* @param boolean $strip_tags_flg => //true  result text is applied strip_tags(), false is not applied it.
+	*
+	* @return str|array
+	*/
+	public function extractSpecifiedTextRange($start, $end, $target_text, $preg_match_all=false, $not_escape=array('start'=>false,'end'=>false), $strip_tags_flg=false){
+		$target_text = preg_replace("/\n/", "", $target_text);
+
+		// escape
+		if($not_escape['start']==false){
+			$start = preg_quote($start, '/');
+		}
+		if($not_escape['end']==false){
+			$end = preg_quote($end, '/');
+		}
+
+		// preg_match
+		if($preg_match_all==true){
+			if(preg_match_all('/'.$start.'(.*?)'.$end.'/', $target_text, $matches)){
+				foreach($matches[1] as $key => $val){
+					$res[$key] = trim($val);
+				}
+			}
+		}else{
+			if(preg_match('/'.$start.'(.*?)'.$end.'/', $target_text, $matches)){
+				$res = trim($matches[1]);
+			}
+		}
+
+		// strip_tags
+		if($strip_tags_flg==true){
+			$res = trim(strip_tags($res));
+		}
+
+		return $res;
+	}
+
+
+
+}
